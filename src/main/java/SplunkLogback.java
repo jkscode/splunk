98a070f2e2b7e4b5d7790e201fd04eb6e90fa44a
Yue Lu <web.yuelu@gmail.com>
2020-06-28 21:27:33 -0400

    init commit

---
+++ src/main/java/SplunkLogback.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SplunkLogbackXml {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("splunkLogger");
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello from SplunkLogbackXml");
+    }
+}
