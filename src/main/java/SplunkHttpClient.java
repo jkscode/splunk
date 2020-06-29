ba19cc5bacd3f1bbb364bdb543a346df19773c0f
jkscode <jkscode@gmail.com>
2020-06-28 21:43:40 -0400

    init commit

--- src/main/java/SplunkHttpClient.java
+++ src/main/java/SplunkHttpClient.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
 
 public class SplunkHttpClient {
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://localhost:8088/services/collector/event");
+
+        String json = "{\"sourcetype\": \"httpclient\", \"event\": \"Hello from SplunkHttpClient\"}";
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+        httpPost.setHeader("Authorization", "Splunk a2048eb6-261c-41ae-925f-ecee4aab6928");
+
+        CloseableHttpResponse response = client.execute(httpPost);
+        System.out.println(response.getStatusLine().getStatusCode());
+        client.close();
+
+
+
+    }
 }
--- src/main/java/SplunkLogback.java
+++ src/main/java/SplunkLogback.java
@@ -1,11 +1,11 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SplunkLogbackXml {
+public class SplunkLogback {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("splunkLogger");
 
     public static void main(String[] args) {
-        LOGGER.info("Hello from SplunkLogbackXml");
+        LOGGER.info("Hello from SplunkLogback");
     }
 }
